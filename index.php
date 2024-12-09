72a3c9ad3df74e912fa6a6c33716cb26d2538b23
aliii390 <alli.said.errahmani@gmail.com>
2024-12-09 16:45:45 +0100

    ADD: fin du style css

--- index.php
+++ index.php
@@ -21,7 +21,8 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Liste Utilisateur</title>
+    <link rel="stylesheet" href="./css/listeUser.css">
 </head>
 
 <body>
